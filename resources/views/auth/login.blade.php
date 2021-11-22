981332d2427063aabe20008f201ba0677670ae5a
Peter Patrick <patricpit10@gmail.com>
2021-11-23 00:12:22 +0100

    login page updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,7 +69,7 @@
                         @if (Route::has('register'))
                         <p class="w-full text-xs text-center text-gray-700 my-6 sm:text-sm sm:my-8">
                             {{ __("Don't have an account?") }}
-                            <a class="text-blue-500 hover:text-blue-700 no-underline hover:underline" href="{{ route('register') }}">
+                            <a class="text-blue-500 hover:text-blue-700 no-underline hover:underline" href="#">
                                 {{ __('Register') }}
                             </a>
                         </p>
